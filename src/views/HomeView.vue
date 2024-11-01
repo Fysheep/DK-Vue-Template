aa2a3b965f32b0e7f8a4fedc9dc3720a7e5b6890
Daniel Kunesch <121219045+Fysheep@users.noreply.github.com>
2024-11-01 18:21:15 +0100

    Cascade from last push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,6 +13,6 @@ export default {
 
 <template>
   <main class="dp-flex fd-c gap-3">
-    <h1 consoleheadline class="ta-center"> Test Header</h1>
+    <h1 class="ta-center"> Test Header</h1>
   </main>
 </template>
